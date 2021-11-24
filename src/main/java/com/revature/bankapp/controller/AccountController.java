4694d602e073ebbfcb648b93094fe07211247eb8
Timothy Harper <leeharperdev@gmail.com>
2021-11-24 10:11:03 -0600

    Update AccountController.java
    Updated: AccountController.java.
    -> Implemented the getUserAccounts() method, which will retrieve a collection of the accounts of the currently logged-in user.
    -> Removed the getAccountBalance() method, as the balance is now calculated on the frontend.
    -> Changed the base route from "account" to "accounts" to conform to the standard of the other routes.

--- src/main/java/com/revature/bankapp/controller/AccountController.java
+++ src/main/java/com/revature/bankapp/controller/AccountController.java
@@ -5,6 +5,7 @@ import com.revature.bankapp.model.Transaction;
 import com.revature.bankapp.model.User;
 import com.revature.bankapp.repository.AccountRepository;
 import com.revature.bankapp.repository.UserRepository;
+import com.revature.bankapp.service.AuthenticationService;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,10 +14,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "account")
+@RequestMapping(path = "accounts")
 public class AccountController {
 
     @Autowired
@@ -26,6 +28,18 @@ public class AccountController {
     @Autowired
     @Setter
     private UserRepository userRepository;
+
+    @Autowired
+    @Setter
+    private AuthenticationService authenticationService;
+
+    @GetMapping()
+    public ResponseEntity<List<Account>> getUserAccounts(@RequestHeader Map<String, String> headers) {
+        User user = this.authenticationService.getUserFromHeaders(headers);
+        user = this.userRepository.findById(user.getId()).get();
+
+        return new ResponseEntity<>(user.getAccounts(), HttpStatus.OK);
+    }
 
     @PostMapping(path = "create", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Account> createNewAccount(@RequestBody Account account) {
@@ -56,17 +70,6 @@ public class AccountController {
             return new ResponseEntity<>(account.get().getTransactions(), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @GetMapping(path = "{id}/balance", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Integer> getAccountBalance(@PathVariable Integer id) {
-        Optional<Account> account = this.accountRepository.findById(id);
-
-        if (account.isPresent()) {
-            return new ResponseEntity<>(account.get().getBalance(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
 }
