0971afe91c6334ed2026a99e24be3616b0adb082
Timothy Harper <leeharperdev@gmail.com>
2021-11-24 10:09:00 -0600

    Update AuthenticationController.java
    Updated: AuthenticationController.java.
    -> Implemented the getSessionInformation() method, which will return session information stored within a JWT.
    -> Removed unused imports.

--- src/main/java/com/revature/bankapp/controller/AuthenticationController.java
+++ src/main/java/com/revature/bankapp/controller/AuthenticationController.java
@@ -2,20 +2,18 @@ package com.revature.bankapp.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.revature.bankapp.model.Person;
-import com.revature.bankapp.model.Role;
 import com.revature.bankapp.model.User;
 import com.revature.bankapp.repository.UserRepository;
 import com.revature.bankapp.service.AuthenticationService;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "auth")
@@ -28,6 +26,20 @@ public class AuthenticationController {
     @Autowired
     @Setter
     private AuthenticationService authenticationService;
+
+    @GetMapping()
+    public ResponseEntity<User> getSessionInformation(@RequestHeader Map<String, String> headers) {
+        String tokenHeader = headers.get("authorization");
+        String token = tokenHeader == null ? "" : tokenHeader.substring(7);
+
+        User user = this.authenticationService.getUserFromHeaders(headers);
+
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
     @PostMapping(path = "login", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> attemptUserLogin(@RequestBody User user) {
